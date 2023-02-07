e2671c9b5e31af6229e72aa1df8c18a2b4dfcf04
lukas.kanka <lukas.kanka@uniprog.cz>
2023-02-07 12:54:50 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,37 @@
+#Nainstalovat do PC:
+
+.NET 7.0 nebo 6.0 s dlouhou podporou
+
+Visual Studio Code
+
+nebo 
+
+Visual Studio 2022 - pouze WIN a MacOS
+
+
+#Potřebná rozšíření:
+
+.NET Extension Pack
+
+C#
+
+Nuget Package Manager
+
+
+
+#Postup vs code: 
+
+
+
+dotnet new xunit - vytvoří consolovou aplikaci
+
+
+
+dotnet add package Selenium.WebDriver - stahne knihovny selenium přejít do složky projektu
+
+
+
+
+
+
+
---
+++ UnitTest1.cs
@@ -0,0 +1,45 @@
+
+using Xunit;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace xUnit.NET
+{
+    public class UnitTest1
+    {
+        String test_url = "https://lukan.cz/";
+        String itemName = "Lukáš Bloguje";
+
+        [Fact]
+        public void Test1()
+        {
+            IWebDriver driver;
+
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(test_url);
+            driver.Manage().Window.Maximize();
+
+            // Zkkontroluje nadpis webu
+            Assert.Equal("Lukáš bloguje - Blog o všem možném i nemožném", driver.Title);
+
+            // Clikne na tlačítko
+            IWebElement addButton = driver.FindElement(By.Id("menu-item-79"));
+            addButton.Click();
+
+            Assert.Equal("O mně - Lukáš bloguje", driver.Title);
+
+            // Počká tři sekundy na další akci
+            Thread.Sleep(3000);
+
+
+
+            Console.Write("Dotestováno");
+
+            // zavře prohlížeč
+            driver.Quit();
+
+        }
+    }
+}
